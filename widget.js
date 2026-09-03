626088caa3a12a699924d36c51375694658c9dbe
taz-abo <taz-abo@users.noreply.github.com>
2026-09-03 06:37:27 +0000

    deploy: 007f745c499a9e6ca6eeb8271ac3a0e6dfa57c50

--- widget.js
+++ widget.js
@@ -100,7 +100,7 @@
 
     const header = el("div", { class: "w-header" });
     const headerLogo = el("img", { src: LOGO_SRC, alt: "taz", class: "w-header-logo" });
-    const headerText = el("span", { class: "w-header-title" }, "Assistent");
+    const headerText = el("span", { class: "w-header-title" }, "tazBot");
     const headerBeta = el("span", { class: "w-beta" }, "BETA");
     header.appendChild(headerLogo);
     header.appendChild(headerText);
